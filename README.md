cc5fec2e77505f14d6c7fbc0c75f6992391ea11a
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2018-05-02 16:56:27 -0400

    FIRST

---
+++ README.md
@@ -0,0 +1,20 @@
+# [![BlazeTheDev](https://i.imgur.com/fgVMXSe.png?1)]()
+
+## License
+| License |
+| :---: |
+| [![BlazinHud License](https://img.shields.io/github/license/iiFlamiinBlaze/HungerDisabler.svg?label=License)](LICENSE) |
+
+Make sure to read all of the license as it could change at anytime.
+
+## Information
+| Download | View Count | Discord |
+| :---: | :---: | :---: |
+ [![Download](https://img.shields.io/badge/download-latest-blue.svg)](https://poggit.pmmp.io/ci/iiFlamiinBlaze/HungerDisabler/) | [![View Count](http://hits.dwyl.io/iiFlamiinBlaze/HungerDisabler.svg)](http://hits.dwyl.io/iiFlamiinBlaze/HungerDisabler) | <a href="https://discord.gg/znEsFsG"><img src="https://discordapp.com/api/guilds/425712766687510528/embed.png" alt="Discord server"/></a> |
+ 
+This plugin allows you to disable hunger through configs. This plugin is for anyone to use.
+If you have any issues with the plugin, please make an issue [here](https://github.com/iiFlamiinBlaze/HungerDisabler/issues/new).
+* Credits: [BlazeTheDev](https://github.com/iiFlamiinBlaze)
+
+## How to use
+For more info about how to use the plugin go to the [HungerDisabler Wiki](https://iiflamiinblaze.github.io/projects/hungerdisabler/).
---
+++ src/iiFlamiinBlaze/HungerDisabler/HungerDisabler.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *  ____  _            _______ _          _____
+ * |  _ \| |          |__   __| |        |  __ \
+ * | |_) | | __ _ _______| |  | |__   ___| |  | | _____   __
+ * |  _ <| |/ _` |_  / _ \ |  | '_ \ / _ \ |  | |/ _ \ \ / /
+ * | |_) | | (_| |/ /  __/ |  | | | |  __/ |__| |  __/\ V /
+ * |____/|_|\__,_/___\___|_|  |_| |_|\___|_____/ \___| \_/
+ *
+ * Copyright (C) 2018 iiFlamiinBlaze
+ *
+ * iiFlamiinBlaze's plugins are licensed under MIT license!
+ * Made by iiFlamiinBlaze for the PocketMine-MP Community!
+ *
+ * @author iiFlamiinBlaze
+ * Twitter: https://twitter.com/iiFlamiinBlaze
+ * GitHub: https://github.com/iiFlamiinBlaze
+ * Discord: https://discord.gg/znEsFsG
+ */
+declare(strict_types=1);
+
+namespace iiFlamiinBlaze\HungerDisabler;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class HungerDisabler extends PluginBase implements Listener{
+
+    const VERSION = "v1.0.0";
+    const PREFIX = TextFormat::AQUA . "HungerDisabler" . TextFormat::GOLD . " > ";
+
+    public function onEnable() : void{
+        $this->getLogger()->info("HungerDisabler " . self::VERSION . " by iiFlamiinBlaze is enabled");
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onExhaust(PlayerExhaustEvent $event) : void{
+        if($this->getConfig()->get("hunger") === "on"){
+            $event->setCancelled(true);
+        }
+    }
+}
